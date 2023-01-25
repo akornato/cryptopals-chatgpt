7e294093fe1c60aca73abc95c13a09df630b462a
Artur Kornatowski <artur.kornatowski@gmail.com>
2023-01-25 16:54:00 +0100

    add solution to 07

---
+++ 07.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+const key = "YELLOW SUBMARINE";
+
+fs.readFile("07.txt", (err, data) => {
+  if (err) throw err;
+  const cipher = crypto.createDecipheriv("aes-128-ecb", key, '');
+  let decrypted = cipher.update(data.toString(), "base64", "utf8");
+  decrypted += cipher.final("utf8");
+  console.log(decrypted);
+});
